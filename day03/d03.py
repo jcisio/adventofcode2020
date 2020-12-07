9a68dea4300c67e3dada6bed36e0f667cdfee417
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-08 00:03:46 +0100

    Day 03 Part 2

--- day03/d03.py
+++ day03/d03.py
@@ -1,15 +1,17 @@
-import re
+import math
 
 f = open('d03.in', 'r')
 
-def findTrees(trees):
+def findTrees(trees, dx, dy):
     c, i, j, l = 0, 0, 0, len(trees[0])
     while i < len(trees):
         if trees[i][j] == '#':
             c += 1
-        i += 1
-        j = (j + 3) % l
+        i += dy
+        j = (j + dx) % l
+    print(c)
     return c
 
 trees = list(f.read().strip().split('\n'))
-print("Puzzle 1: ", findTrees(trees))
+print("Puzzle 1: ", findTrees(trees, 3, 1))
+print("Puzzle 2: ", math.prod(findTrees(trees, dx, dy) for dx, dy in [(1,1), (3,1), (5,1), (7,1), (1,2)]))
