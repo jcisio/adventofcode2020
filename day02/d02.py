b28ad1daa99fabb5a873f23043f6701a9c99ca77
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-07 23:27:10 +0100

    Day 02 Part 2

--- day02/d02.py
+++ day02/d02.py
@@ -11,5 +11,14 @@ def countCorrectPasswords(lines):
             c += 1
     return c
 
+def countCorrectPasswordsV2(lines):
+    c = 0
+    for line in lines:
+        a,b,s,p = re.match(r'^(\d+)-(\d+) ([a-z]): (.*)$', line).groups()
+        if (p[int(a)-1] == s) != (p[int(b)-1] == s):
+            c += 1
+    return c
+
 lines = list(f.read().strip().split('\n'))
 print("Puzzle 1: ", countCorrectPasswords(lines))
+print("Puzzle 2: ", countCorrectPasswordsV2(lines))
