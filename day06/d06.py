3b947ccd1ce6cef30a715d036b43780285cb2d82
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-09 00:10:31 +0100

    Day 06 Part 2

--- day06/d06.py
+++ day06/d06.py
@@ -1,12 +1,25 @@
+import string
+
 f = open('d06.in', 'r')
 
 
-def find_total_yes(groups):
+def find_total_yes_or(groups):
     c = 0
     for group in groups:
         c += len(set(group.replace('\n', '')))
     return c
 
 
+def find_total_yes_and(groups):
+    c = 0
+    for group in groups:
+        s = set(string.ascii_lowercase)
+        for person in group.split('\n'):
+            s = s.intersection(set(person))
+        c += len(s)
+    return c
+
+
 groups = f.read().strip().split('\n\n')
-print("Puzzle 1: ", find_total_yes(groups))
+print("Puzzle 1: ", find_total_yes_or(groups))
+print("Puzzle 2: ", find_total_yes_and(groups))
