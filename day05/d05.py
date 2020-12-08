b1952bab28a149c42b01acc1bbe7f3945395513d
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-08 23:47:21 +0100

    Day 05 Part 2

--- day05/d05.py
+++ day05/d05.py
@@ -14,5 +14,13 @@ def find_highest_seat_id(seats):
     return max([calculate_seat_id(seat) for seat in seats])
 
 
+def find_missing_seat_id(seats):
+    ids = sorted([calculate_seat_id(seat) for seat in seats])
+    for i in range(len(ids)):
+        if ids[i] + 1 < ids[i+1]:
+            return ids[i] + 1
+
+
 seats = list(f.read().strip().split('\n'))
 print("Puzzle 1: ", find_highest_seat_id(seats))
+print("Puzzle 2: ", find_missing_seat_id(seats))
