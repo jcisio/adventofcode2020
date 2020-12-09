edb18c732ff46f2526d8f36cebb8a4be1acce5bd
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-09 15:12:52 +0100

    Day 07 Part 2

--- day07/d07.py
+++ day07/d07.py
@@ -10,8 +10,10 @@ def read_rules(lines):
         if inner == 'no other bags':
             continue
         outer = outer[:-5]# ' bags'
-        inner = inner.split(', ')
-        rules[outer] = set([re.match(r'\d+ (.+?) bags?', e).group(1) for e in inner])
+        rules[outer] = dict()
+        for e in inner.split(', '):
+            match = re.match(r'(\d+) (.+?) bags?', e)
+            rules[outer][match.group(2)] = int(match.group(1))
     return rules
 
 
@@ -21,11 +23,30 @@ def find_shiny_gold(rules):
     while found:
         found = False
         for bag in rules:
-            if bag not in bags and len(bags.intersection(rules[bag])) > 0:
+            if bag not in bags and len(bags.intersection(set(rules[bag].keys()))) > 0:
                 found = True
                 bags.add(bag)
     return len(bags)-1
 
 
-rules = read_rules(f.read().split('\n'))
+def count_bags(rules, bag):
+    content = rules[bag]
+    c = 0
+    while content:
+        bags = list(content.keys())
+        for b in bags:
+            c += content[b]
+            if b in rules:
+                for inner_bag in rules[b]:
+                    count = content[b]*rules[b][inner_bag]
+                    if inner_bag in content:
+                        content[inner_bag] += count
+                    else:
+                        content[inner_bag] = count
+            del content[b]
+    return c
+
+
+rules = read_rules(f.read().strip().split('\n'))
 print("Puzzle 1: ", find_shiny_gold(rules))
+print("Puzzle 2: ", count_bags(rules, 'shiny gold'))
