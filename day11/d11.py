a17f09efdf6053a2419c41fb6e37124251bc25c6
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-12 01:06:22 +0100

    Day 11 Part 2

--- day11/d11.py
+++ day11/d11.py
@@ -1,14 +1,34 @@
 f = open('d11.in', 'r')
 
 
-def stabilizeSeats(seats):
+def countOccupied(seats, j, i):
+    nearby = [(-1,-1), (-1,1), (1,-1), (1,1), (0,1), (0,-1), (1,0), (-1,0)]
+    return sum([1 if seats[j + y][i + x]=='#' else 0 for (x, y) in nearby])
+
+
+def countOccupied2(seats, j, i):
+    nearby = [(-1,-1), (-1,1), (1,-1), (1,1), (0,1), (0,-1), (1,0), (-1,0)]
+    L = len(seats)
+    N = len(seats[0])
+    c = 0
+    for (y, x) in nearby:
+        delta = 1
+        while 0 < j+y*delta < L and  0 < i + x*delta < N:
+            if seats[j+y*delta][i+x*delta] == '.':
+                delta += 1
+                continue
+            elif seats[j+y*delta][i+x*delta] == '#':
+                c += 1
+            break
+    return c
+
+def stabilizeSeats(seats, functionCount, tolerant):
     L = len(seats)
     N = len(seats[0])
     extended_seats = ['.'*(N+2)]
     for n in range(L):
         extended_seats.append('.' + seats[n] + '.')
     extended_seats.append('.'*(N+2))
-    nearby = [(-1,-1), (-1,1), (1,-1), (1,1), (0,1), (0,-1), (1,0), (-1,0)]
     while True:
         changed = False
         new_seats = ['.'*(N+2)]*(L+2)
@@ -16,8 +36,8 @@ def stabilizeSeats(seats):
             for i in range(N):
                 if extended_seats[j+1][i+1] == '.':
                     continue
-                c = sum([1 if extended_seats[j+1+x][i+1+y] == '#' else 0 for (x, y) in nearby])
-                if extended_seats[j+1][i+1] == '#' and c >= 4:
+                c = functionCount(extended_seats, j+1, i+1)
+                if extended_seats[j+1][i+1] == '#' and c >= tolerant:
                     new_seat = 'L'
                     changed = True
                 elif extended_seats[j+1][i+1] == 'L' and c == 0:
@@ -34,4 +54,5 @@ def stabilizeSeats(seats):
 
 
 seats = list(f.read().strip().split('\n'))
-print("Puzzle 1: ", stabilizeSeats(seats))
+print("Puzzle 1: ", stabilizeSeats(seats, countOccupied, 4))
+print("Puzzle 2: ", stabilizeSeats(seats, countOccupied2, 5))
