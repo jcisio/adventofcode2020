6b077db8de4eb37404477b57cc997dd40bcab215
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-16 01:52:50 +0100

    Day 12 Part 2

--- day12/d12.py
+++ day12/d12.py
@@ -20,5 +20,28 @@ def navigate(instructions):
     return abs(coord[0]) + abs(coord[1])
 
 
+def navigate2(instructions):
+    circle = 'NESW'
+    waypoint = [1, 10]
+    coord = [0, 0]
+    for (d, s) in instructions:
+        if d in circle:
+            di = circle.find(d)
+            if di >= 2:
+                di = di - 2
+                s = -s
+            waypoint[di] += s
+        elif d in 'LR':
+            si = s if d == 'R' else 360 - s
+            if si == 90: waypoint = [-waypoint[1], waypoint[0]]
+            elif si == 180: waypoint = [-waypoint[0], -waypoint[1]]
+            elif si == 270: waypoint = [waypoint[1], -waypoint[0]]
+        else:
+            coord[0] += waypoint[0]*s
+            coord[1] += waypoint[1]*s
+    return abs(coord[0]) + abs(coord[1])
+
+
 instructions = [(line[0], int(line[1:])) for line in f.read().strip().split('\n')]
 print("Puzzle 1: ", navigate(instructions))
+print("Puzzle 2: ", navigate2(instructions))
