74d3c73918800f18f13bb258e1944fe21b9c2c7b
Hai-Nam Nguyen <hainam@jcisio.com>
2020-12-25 15:20:08 +0100

    Day 23 Part 2

--- day23/d23.py
+++ day23/d23.py
@@ -1,34 +1,52 @@
 f = open('d23.in', 'r')
 
 
-def makeAMove(circle, current):
-    L = len(circle)
-    cup_current = circle[current]
-    if current <= L-4:
-        new_circle = circle[0:current+1] + circle[current+4:L]
-        extract = circle[current+1:current+4]
-    else:
-        new_circle = circle[4+current-L:current+1]
-        extract = circle[current+1:L] + circle[0:4+current-L]
-    cup_destination = int(cup_current)
+def makeAMove(next_cup, current):
+    a = next_cup[current]
+    b = next_cup[a]
+    c = next_cup[b]
+    dest_cup = current
     while True:
-        cup_destination -= 1
-        if cup_destination == 0:
-            cup_destination = L
-        if str(cup_destination) in new_circle:
-            break
-    destination = new_circle.index(str(cup_destination))
-    new_circle = new_circle[0:destination+1] + extract + new_circle[destination+1:]
-    current = (new_circle.index(cup_current) + 1) % L
-    return new_circle, current
+        dest_cup = dest_cup - 1 if dest_cup > 1 else dest_cup - 1 + len(next_cup)
+        if dest_cup not in [a,b,c]: break
+    next_cup[current] = next_cup[c]
+    next_dest_cup = next_cup[dest_cup]
+    next_cup[dest_cup] = a
+    next_cup[c] = next_dest_cup
+    return next_cup, next_cup[current]
+
+
+def formatCups(next_cup):
+    output = []
+    cup = 1
+    for i in range(len(next_cup)-1):
+        cup = next_cup[cup]
+        output.append(cup)
+    return ''.join(map(str, output))
 
 
 def findPuzzle1(circle):
-    current = 0
-    for i in range(100):
-        circle, current = makeAMove(circle, current)
-    return (circle+circle)[circle.index('1')+1:circle.index('1')+len(circle)]
+    next_cup = dict()
+    for i in range(-1, len(circle)-1):
+        next_cup[circle[i]] = circle[i+1]
+    current = circle[0]
+    for i in range(10):
+        next_cup, current = makeAMove(next_cup, current)
+    return formatCups(next_cup)
 
 
-circle = f.read().strip()
+def findPuzzle2(circle):
+    next_cup = dict()
+    circle += range(10, 1000001)
+    for i in range(-1, len(circle)-1):
+        next_cup[circle[i]] = circle[i+1]
+    current = circle[0]
+    for i in range(10000000):
+        next_cup, current = makeAMove(next_cup, current)
+    print(next_cup[1])
+    return next_cup[1] * next_cup[next_cup[1]]
+
+
+circle = list(map(int, f.read().strip()))
 print("Puzzle 1: ", findPuzzle1(circle))
+print("Puzzle 2: ", findPuzzle2(circle))
